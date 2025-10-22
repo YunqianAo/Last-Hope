0e23d113ba996c01a3879984c56eccfc750fe9ec
YunqianAo <79161526+YunqianAo@users.noreply.github.com>
2025-10-22 22:09:04 +0200

    Create Server

---
+++ MobaServer/MobaServer/Net/UClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobaServer.Net
+{
+    internal class UClient
+    {
+    }
+}
---
+++ MobaServer/MobaServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MobaServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
