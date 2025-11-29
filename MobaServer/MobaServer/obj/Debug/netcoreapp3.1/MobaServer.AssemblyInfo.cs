b493df16a83c33184c618625b658ae606f502c61
YunqianAo <79161526+YunqianAo@users.noreply.github.com>
2025-11-29 21:06:43 +0100

    server

--- MobaServer/MobaServer/obj/Debug/netcoreapp3.1/MobaServer.AssemblyInfo.cs
+++ MobaServer/MobaServer/obj/Debug/netcoreapp3.1/MobaServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MobaServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a19e8b32ecd4f622943500e9d13227ce7d63a2a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+796cbf7284cbb8e1233ca6596cc7ad226b38b721")]
 [assembly: System.Reflection.AssemblyProductAttribute("MobaServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MobaServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
